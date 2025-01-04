3e7d9a99e250ff61e44fa5acc2c4f57ea0a02245
lolwtfpro <122892774+lolwtfpro@users.noreply.github.com>
2025-01-05 00:08:51 +0300

    Create usepot.lua

---
+++ usepot.lua
@@ -0,0 +1,6 @@
+task.wait(20)
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):WaitForChild("Data"):WaitForChild("EquipItem"):InvokeServer("Luck Vial")
+local s, e = pcall(function()
+    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/lolwtfpro/hwaktau/refs/heads/main/usepot.lua"))()')()
+end)
